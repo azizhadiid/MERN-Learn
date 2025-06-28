591bf1802e5afaa935a7675bd63706bb6afcfab5
azizhadiid <azizalhadiid88@gmail.com>
2025-06-28 08:49:30 +0700

    Nodemon & Setting Up Our Routes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
-import express from "express";
+import express, { json } from "express";
 
 const app = express();
 
 app.get("/api/notes", (req,res) => {
     // Send Notes
-    res.status(200).send("You Got 5 Notes");
+    res.status(200).send("You Got 50 Notes");
 });
 
 app.get("/api/notes", (req,res) => {
     // Create Notes
     res.send("You Got 5 Notes");
+});
+
+app.post("/api/notes", (req,res) => {
+    // Send Notes
+    res.status(201).json({message: "Post Create Succesfully"})
+});
+
+app.put("/api/notes/:id", (req,res) => {
+    // Send Notes
+    res.status(200).json({message: "Note Update Succesfully"})
+});
+
+app.delete("/api/notes/:id", (req,res) => {
+    // Send Notes
+    res.status(200).json({message: "Note Delate Succesfully"})
 });
 
 app.listen(5001, () => {
