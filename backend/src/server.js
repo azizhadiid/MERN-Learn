14a6069950d80bad42a2de07ea7b0d53d45b3ad1
azizhadiid <azizalhadiid88@gmail.com>
2025-06-29 08:32:37 +0700

    Middleware & Rate Limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import ratelimier from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -11,6 +12,11 @@ connectDB();
 
 // middleware
 app.use(express.json());
+app.use(ratelimier);
+// app.use((req,res,next) => {
+//     console.log(`Req method is ${req.method} & req URL is ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes", noteRoutes);
 
